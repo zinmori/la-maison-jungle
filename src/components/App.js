08a179d411cbd3aea9d962730f6735d2eeb7ce6a
Ezechiel AGBAN <ezechiel9087@gmail.com>
2023-08-23 22:02:17 +0000

    solution5

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import Banner from './Banner'
-import Cart from './Cart'
-import ShoppingList from './ShoppingList';
+import logo from '../assets/logo.png'
+// import Cart from './Cart'
+import ShoppingList from './ShoppingList'
+import Footer from './Footer'
 
 function App() {
-  return (
-    <div>
-			<Banner />
-			{/*<Cart />*/}
-      <ShoppingList />
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			{/* <Cart /> */}
+			<ShoppingList />
+      <Footer />
 		</div>
-  );
+	)
 }
 
-export default App;
+export default App
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,7 @@
 import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
+
 function ShoppingList() {
 	const categories = plantList.reduce(
 		(acc, plant) =>
@@ -15,11 +17,14 @@ function ShoppingList() {
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-                        {plant.name}
-                        {plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
-                    </li>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
 			</ul>
 		</div>
